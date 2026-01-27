6381e2cd4e45a89dd2feee9d66378e2279feb670
archanaradha95 <archanaradha95@gmail.com>
2026-01-27 19:48:32 +0530

    update

--- Mainsource/src/components/dashboard components/Dashboard_Mainbar.jsx
+++ Mainsource/src/components/dashboard components/Dashboard_Mainbar.jsx
@@ -670,7 +670,7 @@ const handleReset = async () => {
             </div>
             
            {/* dashboard  */}
-            <div className="grid grid-cols-1 lg:grid-cols-2  gap-4 md:gap-6 mt-4 md:mt-8 dashboard-tables">
+            <div className="grid grid-cols-1 lg:grid-cols-2  gap-4 md:gap-6 mt-4  dashboard-tables">
               
               
                 {/* Continuous Absentees */}
