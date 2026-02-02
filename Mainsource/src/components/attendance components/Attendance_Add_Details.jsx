1ce4ab36f6974f2f34fb29453ac6b0f69cc92cbd
moni <mohanapriya.072@gmail.com>
2026-02-02 19:52:26 +0530

    Updated Mobile view

--- Mainsource/src/components/attendance components/Attendance_Add_Details.jsx
+++ Mainsource/src/components/attendance components/Attendance_Add_Details.jsx
@@ -728,7 +728,7 @@ const handleHeaderShiftToggle = (shiftId) => {
   }));
 
   return (
-    <div className="flex flex-col justify-between bg-gray-100 w-screen min-h-screen px-3 md:px-5 pt-2 md:pt-10">
+    <div className="flex flex-col justify-between bg-gray-100 w-full min-h-screen px-3 md:px-5 pt-2 md:pt-10">
       {loading ? (
         <Loader />
       ) : (
@@ -739,20 +739,20 @@ const handleHeaderShiftToggle = (shiftId) => {
             </div>
             <div className="flex justify-start mt-2 md:mt-0 gap-1 items-center ">
               <p
-                className="text:xs md:text-sm text-gray-500  cursor-pointer"
+                className="text-xs md:text-sm text-gray-500  cursor-pointer"
                 onClick={() => navigate("/")}
               >
                 Dashboard
               </p>
               <p>{">"}</p>
               <p
-                className="text:xs md:text-sm text-gray-500  cursor-pointer"
+                className="text-xs md:text-sm text-gray-500  cursor-pointer"
                 onClick={() => navigate("/attendance")}
               >
                 Attendance
               </p>
               <p>{">"}</p>
-              <p className="text:xs md:text-sm    text-[#1ea600]">
+              <p className="text-xs md:text-sm    text-[#1ea600]">
                 Attendance Add
               </p>
             </div>
@@ -781,7 +781,7 @@ const handleHeaderShiftToggle = (shiftId) => {
             </div> */}
 
             <div className="bg-white flex justify-between items-center w-full rounded-2xl shadow-md p-4 md:p-6 mt-5">
-              <div className="flex flex-col md:flex-row gap-5 md:gap-10">
+              <div className="flex flex-row gap-2 md:gap-5">
                 {/* <div className="flex flex-col gap-1">
                   <label className="text-sm font-semibold text-[#6B7280]">
                     Company
@@ -802,7 +802,7 @@ const handleHeaderShiftToggle = (shiftId) => {
                   </select>
                 </div> */}
 
-                <div className="flex flex-col gap-1">
+                <div className="flex flex-col gap-1 w-[40%] md:w-[50%] ">
                   <label className="text-sm font-semibold text-[#6B7280]">
                     Company
                   </label>
@@ -819,7 +819,7 @@ const handleHeaderShiftToggle = (shiftId) => {
                   />
                 </div>
 
-                <div className=" flex flex-col  gap-1 ">
+                <div className="flex flex-col gap-1 w-[40%] md:w-[50%]">
                   <label className="text-sm font-semibold text-[#6B7280]">
                     Date
                   </label>
@@ -829,7 +829,7 @@ const handleHeaderShiftToggle = (shiftId) => {
                     value={selectedDate}
                     onChange={(e)=> setSelectedDate(e.target.value)}
                     // disabled
-                    className="px-2 py-2 rounded-md bg-white border text-[#7C7C7C]"
+                    className="px-4 py-2 rounded-md bg-white border text-[#7C7C7C]"
                   />
                 </div>
               </div>
