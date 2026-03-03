225bc171693b1edd0afb3333713acd541d1daf2f
archanaradha95 <archanaradha95@gmail.com>
2026-03-03 15:26:44 +0530

    updated for checking build

--- Mainsource/src/components/performance report components/Company_Performance_Details.jsx
+++ Mainsource/src/components/performance report components/Company_Performance_Details.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import exportToPDF from '../../Utils/exportToPDF'
 
 const Company_Performance_Details = () => {
   return (
     <div>
-      testing
+                           <button
+  onClick={() =>
+    exportToPDF(
+      getExportData(),
+      popupTitle.replaceAll(" ", "_"),
+      popupTitle
+    )
+  }
+  className="px-3 py-1 rounded bg-white text-red-600 text-sm font-semibold hover:bg-gray-100"
+>
+  PDF
+</button>
+
     </div>
   )
 }
